4eb3a53f45f793369fded1c928813cdaeca23818
Pereirics <joao.lc.pereira1@gmail.com>
2023-04-19 19:00:19 +0100

    Cliente informa o servidor sobre o PID do processo que terminou e também sobre o Timestamp. O Cliente informa o utilizador sobre o tempo de execução do programa em milisegundos

--- cliente.c
+++ cliente.c
@@ -23,12 +23,12 @@ void tokenize(char* comando, char** argv) {
         }
     }
     
-    token = strsep(&cmd, " ");
+    token = strsep(&cmd, " "); //encontra ' ' numa string e torna em '\n';
 
     while (token != NULL && token_number < MAX_TOKENS) {
         argv[token_number] = token;
         //printf("%s!", argv[token_number]);
-        token = strsep(&cmd, " ");
+        token = strsep(&cmd, " "); //strstep function keeps track of the last position in the input string where it found a delimiter character, using a static variable internally.
         token_number++;
     }
 
@@ -37,35 +37,45 @@ void tokenize(char* comando, char** argv) {
 }
 
 void execute(char** comand) {
-    int ret, status, fd;
+    int ret, status, fd, fd1[2];
     char pid_str[32];
     char sec_str[32];
     char usec_str[32]; 
     pid_t pid;
-    struct timeval tv;
+
+    pipe(fd1); // this pipe will be used to send the start time of the command to the parent process.
 
     pid = fork();
     if (pid == 0) {
+        struct timeval tv1;
+
+        fd = open("pipe", O_WRONLY);
+        close(fd1[0]); // we close the read end of the pipe (fd1[0]) since the child process only needs to write to it.
+
         pid_t child_pid;
         child_pid = getpid();
-
-        printf("PID do programa a executar: %d\n", child_pid);
         
-        fd = open("pipe", O_WRONLY);
-    
-        sprintf(pid_str, "%d", child_pid);
-        write(fd, pid_str, strlen(pid_str)+1);
-
-        write(fd, comand[2], strlen(comand[2]));
+        // Indica ao utilizador o PID do programa a executar
+        char pid_str[64]; 
+        int len = snprintf(pid_str, sizeof(pid_str), "PID do programa a executar: %d\n", child_pid);
+        write(1, pid_str, len);
         
-        gettimeofday(&tv, NULL);
+        // Indica ao servidor e ao processo-pai o PID do programa a executar
+        snprintf(pid_str, sizeof(pid_str), "%d\n", child_pid);
+        write(fd, pid_str, strlen(pid_str)); //servidor
+        write(fd1[1], pid_str, strlen(pid_str)); //pai
 
-        sprintf(sec_str, "%ld", tv.tv_sec);
-        sprintf(usec_str, "%ld", tv.tv_usec);
-        write(fd, sec_str, strlen(sec_str)+1);
-        write(fd, usec_str, strlen(usec_str)+1);
+        // Indica ao servidor o nome do programa a executar
+        write(fd, comand[2], sizeof(comand[2]));
+        
+        // Calcula o timestamp e passa o mesmo ao processo-pai e ao servidor
+        gettimeofday(&tv1, NULL);      
+        write(fd1[1], &tv1, sizeof(tv1));
+        write(fd, &tv1, sizeof(tv1));
 
         close(fd);
+        close(fd1[1]);
+
         ret = execvp(comand[2], comand+2);
         if (ret == -1) {
             perror("execvp");
@@ -74,12 +84,35 @@ void execute(char** comand) {
     } else if (pid == -1) {
         perror("fork");
     } else {
+
+        fd = open("pipe", O_WRONLY);
+        close(fd1[1]);
+
         waitpid(pid, &status, 0);
         if (WIFEXITED(status)) {
-            ret = WEXITSTATUS(status);
+            
+            struct timeval start, end;
+            
+            // Lê o PID do programa que acabou de executar e passa ao servidor
+            char buffer[64];
+            int bytes_read = read(fd1[0], buffer, sizeof(buffer));
+            write(fd, buffer, sizeof(bytes_read));
+            
+            // Lê o tempo inicial, calcula o final e calcula a diferença entre os dois (tempo de execução)
+            gettimeofday(&end, NULL);
+            read(fd1[0], &start, sizeof(start));
+            long diff = (end.tv_usec-start.tv_usec)/1000 + (end.tv_sec-start.tv_sec)*1000;
+            
+            // Passa o tempo de execução para o servidor em ms
+            char diff_str[32]; 
+            snprintf(diff_str, sizeof(diff_str), "%ld", diff);
+            write(fd, diff_str, strlen(diff_str));
+
         } else {
             ret = -1;
         }
+
+        close(fd);
     }
 }
 
--- servidor.c
+++ servidor.c
@@ -12,14 +12,14 @@ int main(int argc, char** argv) {
     int res, fd1, fd2, bytes_read;
     char* buffer[4096];
 
-    res = mkfifo("pipe", 0666);
-    
+    res = mkfifo("pipe", 0666); // int mkfifo (const char *pathname, mode_t mode); Creates a custom FIFO file with the name filename "pipe". A file that behaves like a pipe is referred to as a FIFO or a named pipe.
+
     fd1 = open("pipe", O_RDONLY);
     fd2 = open("pipe", O_WRONLY); // Mantém o servidor a correr
 
     while ((bytes_read = read(fd1, buffer, sizeof(buffer))) > 0) {
-        write(1, buffer, bytes_read); 
-    }
+        write(1, buffer, bytes_read); // 1 is standard output device which is the screen
+    }   
 
     close(fd1);
     close(fd2);
