1753f496810eaac3d28d423277bec155201700d7
Pereirics <joao.lc.pereira1@gmail.com>
2023-05-01 11:45:14 +0100

    Função status, falta corrigir o retirar quando o programa termina e falta enviar esses valores pelo pipe para o cliente mostrar ao utilizador.

--- servidor.c
+++ servidor.c
@@ -24,6 +24,10 @@ void status(struct prog store[], int N) {
     gettimeofday(&now, NULL);
 
     for(int i=0; i<N; i++) {
+        /* printf("PID: %d\n", store[i].pid);
+            printf("CMD: %s\n", store[i].cmd);
+            printf("Start: %ld %ld\n", store[i].start.tv_sec, store[i].start.tv_usec);
+            printf("ms: %d\n", store[i].ms);*/
         long diff = (now.tv_usec-store[i].start.tv_usec)/1000 + (now.tv_sec-store[i].start.tv_sec)*1000;
         printf("%d %s %ld ms\n", store[i].pid, store[i].cmd, diff);
     }
@@ -34,7 +38,7 @@ void status(struct prog store[], int N) {
 
 int main(int argc, char** argv) {
 
-    int res, fd1, fd2, fd_ficheiro, bytes_read, i=0;
+    int res, fd1, fd2, bytes_read, i=0;
     struct prog buffer;
     struct prog store[100];
 
@@ -47,31 +51,18 @@ int main(int argc, char** argv) {
         if (!strcmp(buffer.cmd, "status"))
             status(store, i);
         else {
-        int flag = 0, pos;
-        for(int j=0; j<=i; j++) { 
-            if (store[j].pid == buffer.pid) {
-                store[j].ms = buffer.ms;
-                flag = 1;
-                pos = j;
+            int flag = 0, pos;
+            for(int j=0; j<=i; j++) { 
+                if (store[j].pid == buffer.pid) {
+                    store[j] = store[i];
+                    i--;
+                    flag = 1;
             }
         }
-        /*
         if (!flag) {
             store[i] = buffer;
-            printf("PID: %d\n", buffer.pid);
-            printf("CMD: %s\n", buffer.cmd);
-            printf("Start: %ld %ld\n", buffer.start.tv_sec, buffer.start.tv_usec);
-            printf("ms: %d\n", buffer.ms);
+            i++;
         }
-        else {
-            printf("PID: %d\n", store[pos].pid);
-            printf("CMD: %s\n", store[pos].cmd);
-            printf("Start: %ld %ld\n", store[pos].start.tv_sec, store[pos].start.tv_usec);
-            printf("ms: %d\n", store[pos].ms);
-        }
-        */
-        
-        i++;
         }
     }   
 
